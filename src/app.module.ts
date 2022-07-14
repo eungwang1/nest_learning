4b28a25477c70512f7b6e3644498da185ce5e414
eungwang <scm323@naver.com>
2022-07-14 18:13:52 +0900

    fetch : app module & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // .env 사용하려면 import 필요.
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true, // mongodb url을 읽을 수 있도록 설정
+      useUnifiedTopology: true, // 최신 mongodb 드라이버 엔진을 사용하도록 설정
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    mongoose.set('debug', this.isDev);
+  }
+}
